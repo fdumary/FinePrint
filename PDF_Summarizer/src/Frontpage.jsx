b188f3634193b0097bf55e1d26ad7a36686b548f
Gabriel <gabrielp2638@gmail.com>
2025-09-27 15:57:20 -0400

    Added text boxs and other files

---
+++ PDF_Summarizer/src/Frontpage.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function Frontpage(){
+    return(
+        <div className="Frontpginsert">
+            <div>
+            <input name="Paste text here"/>
+            <div></div>
+            <input type="file"></input>
+           </div>
+        
+        </div>
+    )
+}
+
+export default Frontpage
---
+++ PDF_Summarizer/src/Header.jsx
@@ -0,0 +1,15 @@
+
+function header(){
+    return(
+        <header>
+            <h1></h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>How to use</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
